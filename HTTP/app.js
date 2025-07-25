6483c873d18e14353600defe94cd3b7e24441993
Sathvik Devadiga <devadigasathwik88@gmail.com>
2025-07-25 15:07:45 +0530

    modified html

--- HTTP/app.js
+++ HTTP/app.js
@@ -1,6 +1,5 @@
 // http module
 
-
 // const express = require('express')
 // const app = express()
 // const app = require('http')
@@ -9,27 +8,27 @@
 // app.get('/', (req, res) => res.send('Hello World!'))
 // app.get('/', (req, res) => {
 //     res.json({ mssg: " first reques" })
-    
+
 // })
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // const http = require('http')
 // const port = 5000
 
-const http = require('http');
-http.createServer((request, response) => {
-    if (request.url === '/') {
-         response.end(" welcome to home page")
+const http = require("http");
+http
+  .createServer((request, response) => {
+    if (request.url === "/") {
+      response.end(" welcome to home page");
     }
-    if (request.url === '/about') {
-        response.write("welcome to about page")
-        response.end()
+    if (request.url === "/about") {
+      response.write("welcome to about page");
+      response.end();
     }
     // response.writeHead(200,{'content-type' : 'text/html'})
-    response.write(`<h1> no such code<h1><a href="/"> Click Here </a>`)
-    response.end()
-  
-}).listen(8081, () => {
-    console.log('server is running in 8081')
-})
-
+    response.write(`<h1> no such code<h1><a href="/"> Click Here </a>`);
+    response.end();
+  })
+  .listen(8081, () => {
+    console.log("server is not  running in 8081");
+  });
